e5676cbb9c0d2e78ad02b09bd24402f9234209c1
RaazKH <raazkhoshnood@gmail.com>
2022-01-13 10:17:08 +0200

    chore: made button pretty

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import { useState } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+
+const CommonButton = css`
+  border: 0;
+  border-radius: 8px;
+  box-shadow: rgba(151, 65, 252, 0.2) 0 15px 30px -5px;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  font-family: Phantomsans, sans-serif;
+  line-height: 1em;
+  padding: 3px;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  cursor: pointer;
+`;
 
 const Container = styled.div`
   text-align: center;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   background: rgb(2,0,36);
   background: linear-gradient(157deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(5,6,45,1) 100%); 
   color: white;
   height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
 `;
 
 const FlexContainer = styled.div`
+  margin-top: 40px;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
-`;
+  font-family: Phantomsans, sans-serif;
+  font-size: 24px;
+  `;
 
 const StyledInput = styled.input`
   -webkit-appearance: none;
@@ -29,37 +52,22 @@ const StyledInput = styled.input`
 `;
 
 const StyledButton = styled.button`
-  margin-left: 5px;
-
-  align-items: center;
   background-image: linear-gradient(144deg,#AF40FF, #5B42F3 50%,#00DDEB);
-  border: 0;
-  border-radius: 8px;
-  box-shadow: rgba(151, 65, 252, 0.2) 0 15px 30px -5px;
-  box-sizing: border-box;
-  color: #FFFFFF;
+  ${CommonButton};
   display: flex;
-  font-family: Phantomsans, sans-serif;
-  font-size: 20px;
   justify-content: center;
-  line-height: 1em;
   max-width: 100%;
   min-width: 140px;
-  padding: 3px;
-  text-decoration: none;
-  user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-  white-space: nowrap;
-  cursor: pointer;
+  margin-bottom: 50px;
+  font-size: 24px;
 
   :active, :hover {
     outline: 0;
   }
 
-  @media (min-width: 768px) {
-    font-size: 24px;
-    min-width: 196px;
+  @media (min-width: 700px) {
+    margin-left: auto;
+    margin-right: auto;
   }
 `;
 
@@ -67,8 +75,17 @@ const StyledSpan = styled.span`
   background-color: rgb(5, 6, 45);
   padding: 16px 24px;
   border-radius: 6px;
-  width: 100%;
-  height: 100%;
+  transition: 300ms;
+
+  :hover, :active{
+    background: none;
+  }
+`;
+
+const StyledSpans = styled.span`
+  background-color: rgb(5, 6, 45);
+  padding: 0.5px 20px;
+  border-radius: 6px;
   transition: 300ms;
 
   :hover, :active{
@@ -81,13 +98,29 @@ const StyledHeading = styled.h1`
   padding-top: 3rem;
 `;
 
+const StyledRandom = styled.h2`
+  flex-grow: 1;
+  font-size: 16px;
+  font-size: 20vw;
+`;
+
+const StyledSubmit = styled.button`
+  margin: 20px;
+  background-image: linear-gradient(144deg,#AF40FF, #5B42F3 50%,#00DDEB);
+  font-size: 20px;
+  ${CommonButton};
+
+`;
+
 function App() {
   const [number, setNumber] = useState("1");
 
   const [display, setDisplay] = useState("1");
 
   const [disabled, setDisabled] = useState(false);
-  const [randomNumber, setRandomNumber] = useState("");
+  const [reset, setReset] = useState(false);
+
+  const [randomNumber, setRandomNumber] = useState("0");
 
   const [numArray, setNumArray] = useState([]);
 
@@ -103,7 +136,11 @@ function App() {
   }
 
   const submit = () => {
+
+    if (disabled === true) setReset(false);
+
     setDisabled(!disabled);
+
     setDisplay(number);
 
     var fruits = [];
@@ -125,7 +162,7 @@ function App() {
         found = true;
       }
     }
-    if (!(numArray.length > 0)) { setRandomNumber("Good job! Hit Reset to start over"); }
+    if (!(numArray.length > 0)) { setRandomNumber("Good job!"); setReset(true) }
   };
 
   return (
@@ -135,10 +172,10 @@ function App() {
       <FlexContainer>
         <p>Between 1 and&nbsp;</p>
         <StyledInput type="number" disabled={disabled} value={number} onInput={(e) => setNumber(e.target.value)} />
-        <StyledButton onClick={submit}><StyledSpan>{disabled ? "Reset" : "Submit"}</StyledSpan></StyledButton>
+        <StyledSubmit onClick={submit}><StyledSpans>{disabled ? "Reset" : "Submit"}</StyledSpans></StyledSubmit>
       </FlexContainer>
-      <h2>{randomNumber}</h2>
-      <button onClick={generate}>Generate!</button>
+      <StyledRandom>{randomNumber}</StyledRandom>
+      <StyledButton onClick={generate} disabled={!disabled}><StyledSpan>{reset ? "Click Reset!" : disabled ? "Generate!" : "Click Submit!"}</StyledSpan></StyledButton>
     </Container>
   );
 }
